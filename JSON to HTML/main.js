7a226941ea28f51e0485baeb8ffc9576167c2932
Mahade Hasan <mahadehasan88@gmail.com>
2019-10-09 01:08:22 +0600

    JSON to HTML

---
+++ JSON to HTML/main.js
@@ -0,0 +1,34 @@
+const petsData = [
+  {
+    name: "Purrsloud",
+    species: "Cat",
+    favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
+    birthYear: 2017,
+    photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
+  },
+  {
+    name: "Barksalot",
+    species: "Dog",
+    birthYear: 2008,
+    photo: "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
+  },
+  {
+    name: "Meowsalot",
+    species: "Cat",
+    favFoods: ["tuna", "catnip", "celery"],
+    birthYear: 2012,
+    photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
+  }
+];
+
+
+document.getElementById("app").innerHTML = `
+<h1 class="app-title">My Pets: ${petsData.length}</h1>
+${petsData.map(function(pet){
+  return `
+    <div class="animal">
+        <img class="pet-photo" src="${pet.photo}">
+    </div>
+  `;
+}).join(' ')}
+`
