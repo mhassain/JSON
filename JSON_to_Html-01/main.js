f6f264d2b5afa61fd47bb1b4ff4f1f50027ee43b
Mahade Hasan <mahadehasan88@gmail.com>
2019-11-08 02:18:45 +0600

    fatch api

---
+++ JSON_to_Html-01/main.js
@@ -0,0 +1,8 @@
+let jscontent = {
+  firstName: "Md. Mahade",
+  lastName: "hasan"
+};
+
+let output = document.getElementById("output");
+
+output.innerHTML = jscontent.firstName + " " + jscontent.lastName;
